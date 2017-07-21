dea85e0eb303c01150393222338cb0a25d1ef3b4
NileshkumarRaghuvanshi <NileshkumarRaghuvanshi@users.noreply.github.com>
2017-07-21 14:28:52 +0530

    STL code migration

---
+++ STL/vector.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+
+	vector <int> v(10);
+	count << "size of vectore v : "<<v.size()<<endl;
+	//cout << "Nilesh "<<endl;
+	return 0;
+}
+
